9b50df4e9743ef9570d0677a0775e3ece1b2a045
Victor Byrd <vbyrd@singledigits.com>
2022-08-10 10:04:18 -0700

    fix binary names and download strategy

--- contributing.md
+++ contributing.md
@@ -6,7 +6,7 @@ Testing Locally:
 asdf plugin test <plugin-name> <plugin-url> [--asdf-tool-version <version>] [--asdf-plugin-gitref <git-ref>] [test-command*]
 
 #
-asdf plugin test spectral https://github.com/vbyrd/asdf-spectral.git "--version"
+asdf plugin test spectral https://github.com/vbyrd/asdf-spectral.git "spectral --version"
 ```
 
 Tests are automatically run in GitHub Actions on push and PR.
--- lib/utils.bash
+++ lib/utils.bash
@@ -5,7 +5,7 @@ set -euo pipefail
 # TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for spectral.
 GH_REPO="https://github.com/stoplightio/spectral"
 TOOL_NAME="spectral"
-TOOL_TEST="--version"
+TOOL_TEST="spectral --version"
 
 fail() {
   echo -e "asdf-$TOOL_NAME: $*"
@@ -31,8 +31,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if spectral has other means of determining installable versions.
   list_github_tags
 }
 
@@ -41,8 +39,7 @@ download_release() {
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for spectral
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  url="$GH_REPO/releases/download/v${version}/spectral-linux"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -61,7 +58,6 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Assert spectral executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
